15122bc5b94c5e00eb5e276291bfffe7425de927
Drew Rawitz <rawitz@sanbornagency.com>
2017-08-30 11:41:32 -0400

    reorganizing the stories so they live inside the component folder. and setting default config options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,35 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { addDecorator, configure } from '@storybook/react';
+import backgrounds from '@storybook/addon-backgrounds';
+import { withKnobs } from '@storybook/addon-knobs';
+import { setDefaults } from '@storybook/addon-info';
+
+setDefaults({
+  header: false,
+  inline: true
+});
+
+addDecorator((story) => (
+  <div style={{ padding: '15px' }}>
+    {story()}
+  </div>
+));
+
+addDecorator(withKnobs);
+
+addDecorator(
+  backgrounds([
+    { name: 'white', value: '#fff', default: true },
+    { name: 'black', value: '#000' },
+    { name: 'light-gray', value: '#ccc' },
+    { name: 'dark-gray', value: '#999' }
+  ])
+);
+
+const req = require.context('../src/components', true, /.story.js$/);
 
 function loadStories() {
-  require('../stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -1,32 +1,17 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { text, boolean } from '@storybook/addon-knobs';
 import { specs, describe, it } from 'storybook-addon-specifications';
-import backgrounds from '@storybook/addon-backgrounds';
-import Button from '../src/components/Button/Button';
 import { mount } from 'enzyme';
 import expect from 'expect';
+import Button from './Button';
 
 storiesOf('Button', module)
-  .addDecorator(story => (<div style={{padding: '15px'}}>{story()}</div>))
-  .addDecorator(withKnobs)
-  .addDecorator(
-    backgrounds([
-      { name: 'white', value: '#fff', default: true },
-      { name: 'black', value: '#000' },
-      { name: 'light-gray', value: '#ccc' },
-      { name: 'dark-gray', value: '#999' }
-    ])
-  )
   .add(
     'Overview',
-    withInfo({
-      inline: true,
-      header: false
-    })(() => {
+    withInfo('This is a basic button component used throughout the application.')(() => {
       const story = (
         <Button
           disabled={boolean('Disabled', false)}
